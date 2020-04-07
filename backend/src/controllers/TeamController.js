8ce4a4f9d2b46b52a61139aa59c1a98249aa16b3
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-04-07 13:35:58 -0300

    api put (update) working

--- backend/src/controllers/TeamController.js
+++ backend/src/controllers/TeamController.js
@@ -20,7 +20,7 @@ module.exports = {
         const position = req.body.position;
         const country = req.body.country;
         const role = req.body.role;
-        const status = "active";
+        const status = "Active";
 
         const sql = `INSERT INTO team (name, position, country, role, status) VALUES ("${name}","${position}","${country}","${role}","${status}");`; 
 
@@ -42,6 +42,26 @@ module.exports = {
         console.log(id)
 
         const sql = `DELETE FROM team WHERE id = ${id} LIMIT 1;`;  
+
+        connection.query(sql, function (err, result) {
+
+            if (err) throw err;
+
+            return resp.json(result);
+        });
+    },
+
+    update: (req, resp) =>  {
+
+        const name = req.body.name;
+        const position = req.body.position;
+        const country = req.body.country;
+        const role = req.body.role;
+        const status = req.body.status;
+
+        const id = req.body.id;
+
+        const sql = `UPDATE team SET name="${name}", position="${position}", country="${country}", role="${role}", status="${status}" WHERE id = ${id};`;  
 
         connection.query(sql, function (err, result) {
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,9 @@ const TeamController = require('./controllers/TeamController');
 const router = express.Router();
 
 router.get('/', TeamController.index);
+router.put('/', TeamController.update);
 router.post('/', TeamController.create);
 router.delete('/', TeamController.delete);
+
 
 module.exports = router;
